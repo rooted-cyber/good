43c17b8f2ed87176052b79e878de50d7939e1733
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-11-21 21:45:27 +0530

    Add files via upload

---
+++ t.sh
@@ -0,0 +1,9 @@
+apt update
+apt upgrade
+apt remove python
+apt install python
+apt install wget
+wget https://raw.githubusercontent.com/rooted-cyber/MBomb/master/requirements.txt > /dev/null 2>&1
+pip install -r requirements.txt
+cd ~/TBomb
+python3 bomber.py call
