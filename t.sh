e4bf40cb7a16ab0ae03985a315c45e64aecd2c61
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2019-11-24 13:37:13 +0530

    Update t.sh

--- t.sh
+++ t.sh
@@ -3,6 +3,7 @@ apt upgrade
 apt remove python
 apt install python
 apt install wget
+apt imstall curl
 wget https://raw.githubusercontent.com/rooted-cyber/MBomb/master/requirements.txt > /dev/null 2>&1
 pip install -r requirements.txt
 cd ~/TBomb
