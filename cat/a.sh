706fabe4b163a3528ca77dd4544596949b22f6e8
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-12 18:31:19 +0530

    Add files via upload

--- cat/a.sh
+++ cat/a.sh
@@ -1,5 +1,5 @@
 don() {
-	for i in code.py dp.py cmd.py actressdp.py live.py open.py repack.py cmd_list.py phone.py linux.py get_bot.py get_admin.py all.py get_id.py adduser.py dm.py; do
+	for i in help.py code.py dp.py cmd.py actressdp.py live.py open.py repack.py cmd_list.py phone.py linux.py get_bot.py get_admin.py all.py get_id.py adduser.py dm.py; do
 	wget https://raw.githubusercontent.com/rooted-cyber/good/master/cat/$i
 	done
 	}
@@ -15,6 +15,8 @@ don() {
 	printf "\n\n Downloaded and install all plugins\n\n"
 	menu() {
 		cd us*/pl*
+		rm eval*
+		rm help.py
 		don
 		}
 		menu
