e288a90a98fdf1e1c0a7ee742d9ea2c5afeb2625
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-16 05:44:10 +0530

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,27 @@
+don() {
+	for i in img.py my.py help.py code.py dp.py cmd.py actressdp.py live.py open.py repack.py cmd_list.py phone.py linux.py get_bot.py get_admin.py all.py get_id.py adduser.py dm.py; do
+	wget https://raw.githubusercontent.com/rooted-cyber/good/master/cat/$i
+	done
+	}
+	che() {
+		rm ld
+		wget https://raw.githubusercontent.com/rooted-cyber/good/master/cat/ld
+		cat ld
+		}
+		check-bot() {
+			ca=$(cat alive.py | grep -e Cat)
+			if [ -z "$ca" ];then
+			printf "\n\n First install cat userbot !!!\n\n"
+			exit
+			fi
+			}
+	menu() {
+		check-bot
+		che
+		cd us*/pl*
+		rm eval*
+		rm help.py
+		don
+		printf "\n\n Downloaded and install all plugins\n\n"
+		}
+		menu
