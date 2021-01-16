9c19bffe0dd4a7185ef4eaf45e3641e0fd161967
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-16 06:05:59 +0530

    Add files via upload

--- cat/a.sh
+++ cat/a.sh
@@ -1,5 +1,5 @@
 don() {
-	for i in img.py my.py help.py code.py dp.py cmd.py actressdp.py live.py open.py repack.py cmd_list.py phone.py linux.py get_bot.py get_admin.py all.py get_id.py adduser.py dm.py; do
+	for i in ld img.py my.py help.py code.py dp.py cmd.py actressdp.py live.py open.py repack.py cmd_list.py phone.py linux.py get_bot.py get_admin.py all.py get_id.py adduser.py dm.py; do
 	wget https://raw.githubusercontent.com/rooted-cyber/good/master/cat/$i
 	done
 	}
@@ -9,13 +9,22 @@ don() {
 		cat ld
 		}
 		check-bot() {
+			cd us*/pl*
 			ca=$(cat alive.py | grep -e Cat)
 			if [ -z "$ca" ];then
 			printf "\n\n First install cat userbot !!!\n\n"
 			exit
 			fi
 			}
-	printf "\n\n Downloaded and install all plugins\n\n"
+			icheck() {
+				cd us*/pl*
+				if [ -e ld ];then
+				printf "\n\n Downloaded and install all plugins\n\n"
+				else
+				printf "\n\n Not install any plugings !!!\n\n"
+				fi
+				}
+				
 	menu() {
 		check-bot
 		che
@@ -23,5 +32,6 @@ don() {
 		rm eval*
 		rm help.py
 		don
+		icheck
 		}
 		menu
