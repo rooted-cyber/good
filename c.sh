bb8ca14fd1e3f5f170489d8e3558819537ec2f6c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-01-24 07:13:42 +0530

    Add files via upload

--- c.sh
+++ c.sh
@@ -1,9 +1,9 @@
 cd us*/pl*
 ls |grep -c .py
 cl="$(ls | grep -c .py)"
-if [ "$cl" != "351" ];then
+if [ "$cl" != "352" ];then
 printf "\n Update available\n"
-printf "\n\n Update command : `.update now`"
+printf "\n\n Update This"
 else
 printf "\n Up to date\n"
 fi
