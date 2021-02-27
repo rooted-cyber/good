2947a1ad433cd284da23232fa20258a712fa9411
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-27 15:43:51 +0530

    Add files via upload

---
+++ ult/loads.py
@@ -0,0 +1,13 @@
+# Ultroid - UserBot
+@ultroid_cmd(
+    pattern="loads",
+)
+async def mean(event):
+	  	  await event.reply("`.load dp`\n`.load tss`\n`.load check2`\n`.load bots`\n`.load anim`\n`.load em`\n")
+
+
+
+
+
+
+
