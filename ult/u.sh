85e2d3122b119956347dda15f24d94125bfed811
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-27 12:25:33 +0530

    Add files via upload

--- ult/u.sh
+++ ult/u.sh
@@ -1,6 +1,6 @@
 cd addons
-do() {
-	for a in anim.py em.py bots.py tss.py dp.py check2.py screenshot.py;do
+dow() {
+	for a in anim.py em.py bots.py tss.py dp.py check2.py;do
 wget https://raw.githubusercontent.com/rooted-cyber/good/master/ult/$a
 done
 }
@@ -8,11 +8,10 @@ if [ -e check2.py ];then
 printf "Already downloaded files \n\n"
 else
 printf "\n Downloading plugins\n"
-do
+dow
 fi
 if [ -e dp.py ];then
-printf "\n\n
-`.load dp`"
+printf "\n\n`.load dp`"
 fi
 if [ -e tss.py ];then
 printf "\n\n Installing gtts \n\n"
