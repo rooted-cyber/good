7bd6f335e37a197e68069f2cc9f74ed61a364b28
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-02-27 12:04:01 +0530

    Add files via upload

--- ult/u.sh
+++ ult/u.sh
@@ -1,7 +1,10 @@
 cd addons
-for a in anim.py dp.py check2.py screenshot.py;do
+for a in anim.py em.py bots.py tss.py dp.py check2.py screenshot.py;do
 wget https://raw.githubusercontent.com/rooted-cyber/good/master/ult/$a
 done
 if [ -e dp.py ];then
 printf "Downloaded all files"
+pip install gtts
+printf "
+`.load dp`"
 fi
