64429d4aaffd35c81e5cb1854daa549770e4e7f7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-03-01 22:52:14 +0530

    Add files via upload

--- ult/loads.py
+++ ult/loads.py
@@ -3,7 +3,7 @@
     pattern="loads",
 )
 async def mean(event):
-	  	  await event.reply("`.load dp`\n`.load tss`\n`.load check2`\n`.load bots`\n`.load anim`\n`.load em`\n`.load mark`\n")
+	  	  await event.reply("`.load dp`\n`.load tss`\n`.load check2`\n`.load bots`\n`.load anim`\n`.load em`\n`.load mark`\n`.load read`\n")
 
 
 
