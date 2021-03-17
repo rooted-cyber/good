c7228b38b38993afd6e0d19d80929fce52da8ec6
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-03-17 13:26:10 +0530

    Add files via upload

--- ult/a.sh
+++ ult/a.sh
@@ -1,4 +1,4 @@
-down() {
+downn() {
 for a in __pycache__.py fonts.py funts.pyfunctions.py progress.py memeifyhelpers.py progress.py qhelper.py __init__.py;do
 wget https://raw.githubusercontent.com/Sh1vam/javes-3.0/master/ub/helpers/$a
 done
