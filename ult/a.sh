ae43cdbef8e3690d916603c76cd570ed878e52b9
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2021-03-18 10:29:08 +0530

    Update a.sh

--- ult/a.sh
+++ ult/a.sh
@@ -1,5 +1,5 @@
 downn() {
-for a in __pycache__.py fonts.py funts.pyfunctions.py progress.py memeifyhelpers.py progress.py qhelper.py __init__.py;do
+for a in __pycache__.py fonts.py funts.py functions.py progress.py memeifyhelpers.py progress.py qhelper.py __init__.py;do
 wget https://raw.githubusercontent.com/Sh1vam/javes-3.0/master/ub/helpers/$a
 done
 }
